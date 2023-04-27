72580434eff6f44fe1e39fffc1fcb07e4a951f12
Gustavo Souza <84461706+gustavodsds9@users.noreply.github.com>
2023-04-26 22:58:32 -0300

    Add files via upload

---
+++ Program 10.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex10Lis1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double real;
+            double dolar;
+            double ct;
+            double conversao;
+
+            Console.Write("Cotação do Dólar em Reais (R$): ");
+            real = double.Parse(Console.ReadLine());
+            Console.Write("Informe o Dólar (U$): ");
+            dolar = double.Parse(Console.ReadLine());
+
+            conversao = dolar * real;
+            Console.WriteLine("O Valor em Reais em Dólar é {0}", conversao);
+        }
+    }
+}
