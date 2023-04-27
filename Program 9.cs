1d047e3d1612834981cf2d3e7bf8e1f4433856ae
Gustavo Souza <84461706+gustavodsds9@users.noreply.github.com>
2023-04-26 22:58:02 -0300

    Add files via upload

---
+++ Program 9.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex9LIs1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double r;
+            double d;
+            double a;
+
+            Console.Write("Informe o Valor do Diâmetro: ");
+            d = double.Parse(Console.ReadLine());
+            r = d / 2;
+            a = Math.PI*Math.Pow(r, 2);
+            Console.WriteLine("O Valor da Área do Circulo é: {0}", a);
+        }
+    }
+}
