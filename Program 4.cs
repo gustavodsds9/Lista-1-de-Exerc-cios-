e510ab462db0697ce9fc0aab48406d4f197c4f25
Gustavo Souza <84461706+gustavodsds9@users.noreply.github.com>
2023-04-26 22:54:59 -0300

    Add files via upload

---
+++ Program 4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex4Lis1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double b;
+            double h;
+            double a;
+
+            Console.Write("Informe o Valor da Base do Triângulo: ");
+            b = double.Parse(Console.ReadLine());
+            Console.Write("Informe o Valor da Altura do Triângulo: ");
+            h = double.Parse(Console.ReadLine());
+            a = (b * h) / 2;
+            Console.WriteLine("O Valor da Área do Triângulo é: {0}", a);
+        }
+    }
+}
