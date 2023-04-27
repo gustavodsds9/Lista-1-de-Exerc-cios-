4967607de035dd92a4ab1c34e72e8581b22ab6fa
Gustavo Souza <84461706+gustavodsds9@users.noreply.github.com>
2023-04-26 22:59:32 -0300

    Add files via upload

---
+++ Program 11.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex11Lis1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x;
+            double y;
+            double z;
+
+            Console.Write("Informe o Valor de X: ");
+            x=double.Parse(Console.ReadLine());
+            Console.Write("Informe o Valor de Y: ");
+            y =double.Parse(Console.ReadLine());
+            z = Math.Pow(x, y);
+            Console.WriteLine("Resultado: {0}", z);
+        }
+    }
+}
