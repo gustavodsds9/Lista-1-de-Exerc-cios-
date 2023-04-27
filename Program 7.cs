9e8075b2d09f341fb8be2a54656d2dd7c0b15d54
Gustavo Souza <84461706+gustavodsds9@users.noreply.github.com>
2023-04-26 22:56:42 -0300

    Add files via upload

---
+++ Program 7.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex7Lis1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double milha;
+            double km;
+
+            Console.Write("Digite o Valor em Milhas Marítimas da Distância: ");
+            milha = double.Parse(Console.ReadLine());
+            km = (milha * 1852 / 1000);
+            Console.WriteLine("O Valor em Km é: {0}", km);
+        }
+    }
+}
