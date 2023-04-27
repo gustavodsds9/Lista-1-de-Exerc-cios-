d56cb2fb9a8d385a056dab5297774884d637bd85
Gustavo Souza <84461706+gustavodsds9@users.noreply.github.com>
2023-04-26 22:54:16 -0300

    Add files via upload

---
+++ Program 3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex3Li1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double d;
+            double a;
+
+            Console.Write("Informe o Valor da Diagonal: ");
+            d = double.Parse(Console.ReadLine());
+            a = Math.Pow(d, 2) / 2;
+            Console.WriteLine("O Valor da Área do Quadrado é: {0}", a);
+        }
+    }
+}
