f2907b0e27b22f5f932bdccb8c249a4d6a5d8336
Gustavo Souza <84461706+gustavodsds9@users.noreply.github.com>
2023-04-26 22:51:20 -0300

    Add files via upload

---
+++ Program 1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1Lis1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int b;
+            int h;
+            int a;
+
+            Console.Write("Informe o valor da base: ");
+            b = int.Parse(Console.ReadLine());
+            Console.Write("Informe o valor da altura: ");
+            h = int.Parse(Console.ReadLine());
+            a = b * h;
+            Console.WriteLine("O valor da área do retângulo é: {0}", a);
+        }
+    }
+}
