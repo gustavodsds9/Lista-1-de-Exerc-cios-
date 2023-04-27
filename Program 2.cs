7c1e6e92e007e4a4de359640be14e9748a277119
Gustavo Souza <84461706+gustavodsds9@users.noreply.github.com>
2023-04-26 22:52:49 -0300

    Add files via upload

---
+++ Program 2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2Lis1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double l1;
+            double l2;
+            double a;
+
+            Console.Write("Informe o Valor da Aresta: ");
+            l1 = double.Parse(Console.ReadLine());
+            l2 = l1;
+            a = l1 * l2;
+            Console.WriteLine("O Valor da Área do Quadrado é: {0}", a);
+
+        }
+    }
+}
