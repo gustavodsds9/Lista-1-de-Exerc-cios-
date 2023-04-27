1819094fe3e335e57bc86e85403838a42265f187
Gustavo Souza <84461706+gustavodsds9@users.noreply.github.com>
2023-04-26 22:57:29 -0300

    Add files via upload

---
+++ Program 8.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex8Lis1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double c;
+            double f;
+
+            Console.Write("Informe o Valor da Temperatura em Graus Celsius ºC: ");
+            c = double.Parse(Console.ReadLine());
+            f = ((c / 5) * 9 + 32);
+            Console.WriteLine("O Valor da Temperatura em Fahrenheit ºF é: {0}", f);
+        }
+    }
+}
